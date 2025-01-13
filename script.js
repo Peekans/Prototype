a2303ca6d507ba8e225a3bb90dcaf2ded276381c
peekan <192701619+Peekans@users.noreply.github.com>
2025-01-13 03:29:14 -0800

    Creating a the base for the prototype of website

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const signupButton = document.getElementById('signupButton');
+    
+    signupButton.addEventListener('click', function() {
+        window.location.href = './signup/signup.html';
+    });
+});
